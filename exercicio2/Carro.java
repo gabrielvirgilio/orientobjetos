91337988662997b566bccbc5c9d51e3b480006f7
Gabriel Virgílio <131073636+gabrielvirgilio@users.noreply.github.com>
2024-09-19 21:28:18 -0300

    Create Carro.java

---
+++ exercicio2/Carro.java
@@ -0,0 +1,13 @@
+// Subclasse Carro
+public class Carro extends Veiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("O carro está acelerando.");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("O carro está freando.");
+    }
+}
