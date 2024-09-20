b4c82e1cb18e608611d3a6ade6fca6d53b0155de
Gabriel Virgílio <131073636+gabrielvirgilio@users.noreply.github.com>
2024-09-19 21:14:40 -0300

    Create main.java

---
+++ exercicio2/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto Estudante
+        Estudante estudante1 = new Estudante("João", "190924");
+
+        // Criando um objeto Curso
+        Curso curso = new Curso("Agronomia");
+
+        // Matriculando o estudante no curso
+        cursoJava.matricularEstudante(estudante1);
+    }
+}
