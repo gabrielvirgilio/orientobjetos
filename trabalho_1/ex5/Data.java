2db6ed25480c2f6119f8902e3a077e6125253e6a
Gabriel Virgílio <131073636+gabrielvirgilio@users.noreply.github.com>
2024-10-10 00:10:04 -0300

    Create Data.java

---
+++ trabalho_1/ex5/Data.java
@@ -0,0 +1,32 @@
+package cinema;
+
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    // Construtor
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    // Método Get
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    // Calcular idade com base na data atual
+    public int calcularIdade(int anoAtual) {
+        return anoAtual - this.ano;
+    }
+}
